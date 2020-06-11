5e426008d84ad354ae4362416b3a1e5739f89601
wangliu1102 <wangliu.ah@qq.com>
2020-06-11 15:51:39 +0800

    添加课程-第三步：最终发布

--- src/api/edu/course.js
+++ src/api/edu/course.js
@@ -30,5 +30,19 @@ export default {
       method: 'post',
       data: courseInfo
     })
+  },
+  //课程确认信息显示
+  getPublishCourseInfo(id) {
+    return request({
+      url: '/eduservice/course/getPublishCourseInfo/' + id,
+      method: 'get'
+    })
+  },
+  //课程最终发布
+  publishCourse(id) {
+    return request({
+      url: '/eduservice/course/publishCourse/' + id,
+      method: 'post'
+    })
   }
 }
--- src/views/edu/course/publish.vue
+++ src/views/edu/course/publish.vue
@@ -10,6 +10,17 @@
       <el-step title="最终发布"/>
     </el-steps>
 
+    <div class="ccInfo">
+      <img :src="coursePublish.cover">
+      <div class="main">
+        <h2>{{ coursePublish.title }}</h2>
+        <p class="gray"><span>共{{ coursePublish.lessonNum }}课时</span></p>
+        <p><span>所属分类：{{ coursePublish.subjectLevelOne }} — {{ coursePublish.subjectLevelTwo }}</span></p>
+        <p>课程讲师：{{ coursePublish.teacherName }}</p>
+        <h3 class="red">￥{{ coursePublish.price }}</h3>
+      </div>
+    </div>
+
     <el-form label-width="120px">
 
       <el-form-item>
@@ -21,28 +32,104 @@
 </template>
 
 <script>
+  import course from '@/api/edu/course'
 
   export default {
     data() {
       return {
-        saveBtnDisabled: false // 保存按钮是否禁用
+        saveBtnDisabled: false, // 保存按钮是否禁用
+        courseId: '', // 所属课程id
+        coursePublish: {}
       }
     },
-
     created() {
-      console.log('publish created')
+      this.init()
     },
 
     methods: {
-      previous() {
-        console.log('previous')
-        this.$router.push({ path: '/course/chapter/1' })
+      init() {
+        if (this.$route.params && this.$route.params.id) {
+          this.courseId = this.$route.params.id
+          // 根据id获取课程基本信息
+          this.fetchCoursePublishInfoById()
+        }
       },
 
+      fetchCoursePublishInfoById() {
+        course.getPublishCourseInfo(this.courseId).then(response => {
+          this.coursePublish = response.data.item
+        })
+      },
+      previous() {
+        console.log('previous')
+        this.$router.push({ path: '/course/chapter/' + this.courseId })
+      },
       publish() {
-        console.log('publish')
-        this.$router.push({ path: '/course/list' })
+        course.publishCourse(this.courseId).then(response => {
+          //提示
+          this.$message({
+            type: 'success',
+            message: '课程发布成功!'
+          })
+          this.$router.push({ path: '/course/list' })
+        })
       }
     }
   }
 </script>
+<style scoped>
+  .ccInfo {
+    background: #f5f5f5;
+    padding: 20px;
+    overflow: hidden;
+    border: 1px dashed #DDD;
+    margin-bottom: 40px;
+    position: relative;
+  }
+
+  .ccInfo img {
+    background: #d6d6d6;
+    width: 500px;
+    height: 278px;
+    display: block;
+    float: left;
+    border: none;
+  }
+
+  .ccInfo .main {
+    margin-left: 520px;
+  }
+
+  .ccInfo .main h2 {
+    font-size: 28px;
+    margin-bottom: 30px;
+    line-height: 1;
+    font-weight: normal;
+  }
+
+  .ccInfo .main p {
+    margin-bottom: 10px;
+    word-wrap: break-word;
+    line-height: 24px;
+    max-height: 48px;
+    overflow: hidden;
+  }
+
+  .ccInfo .main p {
+    margin-bottom: 10px;
+    word-wrap: break-word;
+    line-height: 24px;
+    max-height: 48px;
+    overflow: hidden;
+  }
+
+  .ccInfo .main h3 {
+    left: 540px;
+    bottom: 20px;
+    line-height: 1;
+    font-size: 28px;
+    color: #d32f24;
+    font-weight: normal;
+    position: absolute;
+  }
+</style>
