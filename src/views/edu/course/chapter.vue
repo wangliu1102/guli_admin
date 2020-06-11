31f76d5c1acb52df7b2f02fd6203b9a08530b794
wangliu1102 <wangliu.ah@qq.com>
2020-06-11 11:45:54 +0800

    添加课程-第二步：课程大纲列表显示

--- src/views/edu/course/chapter.vue
+++ src/views/edu/course/chapter.vue
@@ -10,6 +10,41 @@
       <el-step title="最终发布"/>
     </el-steps>
 
+    <el-button type="text" @click="openChapterDialog()">添加章节</el-button>
+
+    <!-- 章节 -->
+    <ul class="chanpterList">
+      <li
+        v-for="chapter in chapterVideoList"
+        :key="chapter.id">
+        <p>
+          {{ chapter.title }}
+
+          <span class="acts">
+                    <el-button style="" type="text" @click="openVideo(chapter.id)">添加小节</el-button>
+                    <el-button style="" type="text" @click="openEditChatper(chapter.id)">编辑</el-button>
+                    <el-button type="text" @click="removeChapter(chapter.id)">删除</el-button>
+                </span>
+        </p>
+
+        <!-- 视频 -->
+        <ul class="chanpterList videoList">
+          <li
+            v-for="video in chapter.children"
+            :key="video.id">
+            <p>{{ video.title }}
+
+              <span class="acts">
+
+                    <el-button style="" type="text">编辑</el-button>
+                    <el-button type="text" @click="removeVideo(video.id)">删除</el-button>
+                </span>
+            </p>
+          </li>
+        </ul>
+      </li>
+    </ul>
+
     <el-form label-width="120px">
 
       <el-form-item>
@@ -20,14 +55,23 @@
   </div>
 </template>
 <script>
+  import chapter from '@/api/edu/chapter'
+
   export default {
     data() {
       return {
-        saveBtnDisabled: false
+        saveBtnDisabled: false,
+        courseId: '',//课程id
+        chapterVideoList: []
       }
     },
     created() {
-
+      //获取路由的id值
+      if (this.$route.params && this.$route.params.id) {
+        this.courseId = this.$route.params.id
+        //根据课程id查询章节和小节
+        this.getChapterVideo()
+      }
     },
     methods: {
       previous() {
@@ -36,7 +80,54 @@
       next() {
         //跳转到第二步
         this.$router.push({ path: '/course/publish/1' })
+      },
+      //根据课程id查询章节和小节
+      getChapterVideo() {
+        chapter.getAllChapterVideo(this.courseId)
+          .then(response => {
+            this.chapterVideoList = response.data.allChapterVideo
+          })
       }
     }
   }
 </script>
+<style scoped>
+  .chanpterList{
+    position: relative;
+    list-style: none;
+    margin: 0;
+    padding: 0;
+  }
+  .chanpterList li{
+    position: relative;
+  }
+  .chanpterList p{
+    float: left;
+    font-size: 20px;
+    margin: 10px 0;
+    padding: 10px;
+    height: 70px;
+    line-height: 50px;
+    width: 100%;
+    border: 1px solid #DDD;
+  }
+  .chanpterList .acts {
+    float: right;
+    font-size: 14px;
+  }
+
+  .videoList{
+    padding-left: 50px;
+  }
+  .videoList p{
+    float: left;
+    font-size: 14px;
+    margin: 10px 0;
+    padding: 10px;
+    height: 50px;
+    line-height: 30px;
+    width: 100%;
+    border: 1px dotted #DDD;
+  }
+
+</style>
