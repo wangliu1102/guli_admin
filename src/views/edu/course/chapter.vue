dbc5ffc86f27432f11b27f055bd136a35597a465
wangliu1102 <wangliu.ah@qq.com>
2020-06-12 15:15:09 +0800

    添加课程-第二步：课程大纲-》添加小节中视频上传功能

--- src/views/edu/course/chapter.vue
+++ src/views/edu/course/chapter.vue
@@ -84,7 +84,23 @@
           </el-radio-group>
         </el-form-item>
         <el-form-item label="上传视频">
-          <!-- TODO -->
+          <el-upload
+            :on-success="handleVodUploadSuccess"
+            :on-exceed="handleUploadExceed"
+            :file-list="fileList"
+            :action="BASE_API+'/eduvod/video/uploadAlyiVideo'"
+            :limit="1"
+            class="upload-demo">
+            <el-button size="small" type="primary">上传视频</el-button>
+            <el-tooltip placement="right-end">
+              <div slot="content">最大支持1G，<br>
+                支持3GP、ASF、AVI、DAT、DV、FLV、F4V、<br>
+                GIF、M2T、M4V、MJ2、MJPEG、MKV、MOV、MP4、<br>
+                MPE、MPG、MPEG、MTS、OGG、QT、RM、RMVB、<br>
+                SWF、TS、VOB、WMV、WEBM 等视频格式上传</div>
+              <i class="el-icon-question"/>
+            </el-tooltip>
+          </el-upload>
         </el-form-item>
       </el-form>
       <div slot="footer" class="dialog-footer">
@@ -116,7 +132,9 @@
         },
         dialogChapterFormVisible: false,//章节弹框
         dialogVideoFormVisible: false,//小节弹框
-        saveVideoBtnDisabled: false
+        saveVideoBtnDisabled: false,
+        fileList: [],//上传文件列表
+        BASE_API: process.env.BASE_API // 接口API地址
       }
     },
     created() {
@@ -141,6 +159,16 @@
           .then(response => {
             this.chapterVideoList = response.data.allChapterVideo
           })
+      },
+      //上传视频成功调用的方法
+      handleVodUploadSuccess(response, file, fileList) {
+        //上传视频id赋值
+        this.video.videoSourceId = response.data.videoId
+        //上传视频名称赋值
+        this.video.videoOriginalName = file.name
+      },
+      handleUploadExceed() {
+        this.$message.warning('想要重新上传视频，请先删除已上传的视频')
       },
       //==============================章节操作====================================
       //删除章节
